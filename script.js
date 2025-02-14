bb29041e680bb0959b9b87586d09136ba1b544e3
Rajveer1496 <rajveer14961496@gmail.com>
2025-02-15 00:07:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // script.js
 const leaderboardData = [
+
+  { team: "TEAM SD Warriors", chickens: 0,place: 14, kills: 24, points: 0 }, //done
+
+  { team: "Team Zero", chickens: 1, kills: 21,place: 0, points: 0 }, //done
   
   { team: "BINARY 4", chickens: 1, kills: 20,place: 0, points: 0 }, //done
 
-  { team: "Apsarao ke patidev", chickens: 1,place: 0, kills: 11, points: 0 }, //done
+  { team: "Apsarao ke patidev", chickens: 1,place: 6, kills: 14, points: 0 }, //done
 
-  { team: "TEAM SD Warriors", chickens: 0,place: 6, kills: 10, points: 0 }, //done
+  { team: "XSpark", chickens: 0, kills: 10,place: 6, points: 0 }, //done
 
-  { team: "Team Zero", chickens: 0, kills: 13,place: 0, points: 0 }, //done
+  { team: "TokTok Gang", chickens: 0, kills: 6,place: 8, points: 0 }, //done
 
-  { team: "TokTok Gang", chickens: 0, kills: 5,place: 8, points: 0 }, //done
+  { team: "The.બાઝ", chickens: 0, kills: 11,place: 2, points: 0 }, //done
 
-  { team: "Maqsad", chickens: 0, kills: 3,place: 8, points: 0 }, //done
+  { team: "Maqsad", chickens: 0, kills: 4,place: 8, points: 0 }, //done
 
-  { team: "The.બાઝ", chickens: 0, kills: 7,place: 2, points: 0 }, //done
-
-  { team: "XSpark", chickens: 0, kills: 3,place: 6, points: 0 }, //done
-
-  { team: "Death", chickens: 0, kills: 0,place: 6, points: 0 }, //done
+  { team: "Death", chickens: 0, kills: 1,place: 10, points: 0 }, //done
 
   { team: "Team Chatakapataka", chickens: 0, kills: 0,place: 4, points: 0 }, //done
 
+  { team: "X-Factor Elite", chickens: 0, kills: 1,place: 2, points: 0 }, //done
+
   { team: "MAUT KE SAUDAGAR", chickens: 0, kills: 1,place: 0, points: 0 }, //done
   
-  { team: "X-Factor Elite", chickens: 0, kills: 0,place: 0, points: 0 }, //done
-
   
   // { team: "", chickens: 0, kills: 0,place: 0, points: 0 },
 ];
