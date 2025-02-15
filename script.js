4a82994a3de0bec3958459252be9af5d5c7b67da
Rajveer1496 <rajveer14961496@gmail.com>
2025-02-15 23:29:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 // script.js
 const leaderboardData = [
 
-  { team: "Team Zero", chickens: 1, kills: 34,place: 6, points: 0 }, //done
+  { team: "Team Zero", chickens: 1, kills: 45,place: 14, points: 0 }, //done
 
-  { team: "Apsarao ke patidev", chickens: 1,place: 14, kills: 21, points: 0 }, //done
+  { team: "TEAM SD Warriors", chickens: 0,place: 20, kills: 32, points: 0 }, //done
 
-  { team: "TEAM SD Warriors", chickens: 0,place: 14, kills: 28, points: 0 }, //done
+  { team: "Apsarao ke patidev", chickens: 1,place: 14, kills: 26, points: 0 }, //done
   
-  { team: "BINARY 4", chickens: 1, kills: 24,place: 0, points: 0 }, //done
+  { team: "BINARY 4", chickens: 1, kills: 25,place: 0, points: 0 }, //done
 
-  { team: "XSpark", chickens: 0, kills: 12,place: 8, points: 0 }, //done
+  { team: "The.બાઝ", chickens: 1, kills: 21,place: 2, points: 0 }, //done
 
-  { team: "The.બાઝ", chickens: 0, kills: 18,place: 2, points: 0 }, //done
+  { team: "Maqsad", chickens: 0, kills: 15,place: 12, points: 0 }, //done
 
-  { team: "Team Chatakapataka", chickens: 1, kills: 4,place: 4, points: 0 }, //done
+  { team: "XSpark", chickens: 0, kills: 16,place: 8, points: 0 }, //done
 
-  { team: "Maqsad", chickens: 0, kills: 8,place: 8, points: 0 }, //done
+  { team: "Team Chatakapataka", chickens: 1, kills: 5,place: 4, points: 0 }, //done
 
   { team: "Death", chickens: 0, kills: 2,place: 14, points: 0 }, //done
 
-  { team: "TokTok Gang", chickens: 0, kills: 6,place: 8, points: 0 }, //done
+  { team: "TokTok Gang", chickens: 0, kills: 7,place: 8, points: 0 }, //done
 
-  { team: "X-Factor Elite", chickens: 0, kills: 1,place: 2, points: 0 }, //done
+  { team: "Team Rookie", chickens: 0, kills: 12,place: 0, points: 0 }, //done
+
+  { team: "X-Factor Elite", chickens: 0, kills: 1,place: 4, points: 0 }, //done
 
   { team: "MAUT KE SAUDAGAR", chickens: 0, kills: 1,place: 0, points: 0 }, //done
-
-  { team: "Team Rookie", chickens: 0, kills: 0,place: 0, points: 0 }, //done
-  
   
   // { team: "", chickens: 0, kills: 0,place: 0, points: 0 },
 ];
